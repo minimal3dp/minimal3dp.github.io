1c85cd2fb86ef6ae18e073811603f1521f9c1b79
Mike Wilson <wilsongis@gmail.com>
2025-11-12 21:49:07 -0600

    Move build logic to build.sh script (vercel.json char limit)

--- build.sh
+++ build.sh
@@ -2,15 +2,18 @@
 set -e
 
 echo "Installing Go..."
-curl -L https://go.dev/dl/go1.21.5.linux-amd64.tar.gz -o go.tar.gz
-tar -C /tmp -xzf go.tar.gz
+curl -L https://go.dev/dl/go1.21.5.linux-amd64.tar.gz | tar -C /tmp -xzf -
 export PATH="/tmp/go/bin:$PATH"
 export GOPATH="/tmp/go"
 
-echo "Go version:"
-go version
+echo "Installing Hugo..."
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.152.2/hugo_extended_0.152.2_linux-amd64.tar.gz | tar -xzf - -C /tmp
+
+echo "Verifying installations..."
+/tmp/go/bin/go version
+/tmp/hugo version
 
 echo "Building Hugo site..."
-hugo --gc --minify
+/tmp/hugo --gc --minify
 
 echo "Build complete!"
